7f7c209ad3287e7ab5d8d6d13b80ecb536de8b00
n-s-harvey <150607031+n-s-harvey@users.noreply.github.com>
2023-11-26 10:52:09 -0600

    Updated Cash Register project.

--- 02_JavaScript_Algorithm_and_Data_Structures/09_Projects/05_cash_register.js
+++ 02_JavaScript_Algorithm_and_Data_Structures/09_Projects/05_cash_register.js
@@ -1,5 +1,6 @@
 /* 
-Design a cash register drawer function checkCashRegister() that accepts purchase price as the first argument (price), payment as the second argument (cash), and cash-in-drawer (cid) as the third argument.
+Design a cash register drawer function checkCashRegister() that accepts purchase price 
+as the first argument (price), payment as the second argument (cash), and cash-in-drawer (cid) as the third argument.
 
 cid is a 2D array listing available currency.
 
@@ -12,22 +13,124 @@ Return {status: "CLOSED", change: [...]} with cash-in-drawer as the value for th
 Otherwise, return {status: "OPEN", change: [...]}, with the change due in coins and bills, sorted in highest to lowest order, as the value of the change key.
  */
 
-function checkCashRegister(price, cash, cid) {
+/* 
+* Things to consider:
+*   price == cash
+*   how to determine correct amount of change?
+*     - maybe start with highest denomination & go down
+* 
+* {status: "INSUFFICIENT_FUNDS", change: []}
+* {status: "CLOSED", change: [...]} // if CID == change
+* {status: "OPEN", change: [...]}
+* 
+* 
+ */
 
-  /* 
-  * Things to consider:
-  *   price == cash
-  *   how to determine correct amount of change?
-  *     - maybe start with highest denomination & go down
-  * 
-  * {status: "INSUFFICIENT_FUNDS", change: []}
-  * {status: "CLOSED", change: [...]} // if CID == change
-  * {status: "OPEN", change: [...]}
-  * 
-  * 
+/*
+ *
+ * How to determine correct amount of change?
+ * 1. Subtracting by the largest possible denomination that is less than or equal to amount owed
+ * 2. Reduce CID by amount from step 1
+ * 3. Repeat (recursive function? - base case: 0 balance OR no change left in drawer)
+ * 
+ */
+class Currencies {
+  static values = new Map([
+    ["ONE HUNDRED", 100],
+    ["TWENTY", 20],
+    ["TEN", 10],
+    ["FIVE", 5],
+    ["ONE", 1],
+    ["QUARTER", 0.25],
+    ["DIME", 0.10],
+    ["NICKEL", 0.05],
+    ["PENNY", 0.01],
+  ]);
+}
+
+class Denomination {
+  #_value;
+  name;
+  constructor(name) {
+    /**
+     * @type {String}
+     */
+    this.name = name;
+    /**
+    * @type {Number}
+    */
+    this.value = Currencies.values.get(name);
+
+  }
+  get value() {
+    return this.#_value;
+  }
+  set value(amount) {
+    this.#_value = amount;
+  }
+}
+
+class Drawer {
+  /**
+   * @type {{currency: Denomination, amount: number}[]}
    */
-  
+  #cash = [];
+  /**
+   * @param {Array<Array>} cid 
+   */
+  constructor(cid) {
+    this.#cash = cid.map(
+      element => {
+        let name = element[0];
+        let amount = element[1];
+        return {
+          currency: new Denomination(name),
+          amount: amount
+        }
+      }
+    )
+  }
+  getBalance(currencyName) {
+    for (const element of this.#cash) {
+      if (element.currency.name == currencyName) return element.amount;
+    }
+    return undefined;
+  }
+  /**
+   * @param {string} currencyName 
+   * @param {number} amount 
+   */
+  addToBalance(currencyName, amount) {
+    for (const element of this.#cash) {
+      if (element.currency.name == currencyName) {
+        element.amount += amount;
+      }
+    }
+  }
+}
+
+let penny = new Denomination("PENNY");
+console.log(penny.value);
+function checkCashRegister(price, cash, cid) {
   let change;
+  let balance = price;
+  let drawer = new Drawer(cid);
+
+  while (balance > 0) {
+
+  }
+
+  /**
+   * @param {number} amount 
+   * @param {Drawer} drawer 
+   */
+  function reduceBalance(amount, drawer) {
+    for (const currency of Currencies.values) {
+      if (drawer.getBalance(currency) > 0) {
+        drawer.addToBalance(-amount);
+      }
+    }
+  }
   return change;
 }
 
